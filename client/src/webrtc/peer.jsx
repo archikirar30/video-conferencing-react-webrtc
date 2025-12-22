8716e793320cec27248f9ef55da93bbfbe451a67
Archi kirar <archikirar@gmail.com>
2025-12-22 22:27:35 +0530

    Change the configuration for the app

--- client/src/webrtc/peer.jsx
+++ client/src/webrtc/peer.jsx
@@ -8,7 +8,12 @@ export function createPeerConnection({
 }) {
   const pc = new RTCPeerConnection({
     iceServers: [
-      { urls: "stun:stun.l.google.com:19302" }
+      { urls: "stun:stun.l.google.com:19302" },
+      {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    }
     ]
   });
 
