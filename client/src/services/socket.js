50484f2315034c39bb94a53020fe179f6407d158
Archi kirar <archikirar@gmail.com>
2025-12-22 22:38:12 +0530

    added the additional config

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,3 +1,5 @@
 import {io} from "socket.io-client"
 
-export const socket = io(import.meta.env.VITE_SIGNALING_URL);
+export const socket = io(import.meta.env.VITE_SIGNALING_URL, {
+  transports: ["websocket"]
+});
